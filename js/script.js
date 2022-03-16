3efabf101996d87ac87513c685d9911fbc582881
MattiaPortanti <mattia.portantipm@gmail.com>
2022-03-16 17:42:43 +0100

    aggiunti data con messaggio da stampare nel DOM e immagine

--- js/script.js
+++ js/script.js
@@ -1,6 +1,13 @@
 //todo Creata variabile assegnata ad una nuova classe Vue e con all'interno oggetto con proprieta El
-const app = new Vue(
-    {
-        el: "#root"   //*Il contenuto della proprietà El è l'id dell'elemento di partenza di Vue
+const app = new Vue({
+
+    el: "#root",   //*Il contenuto della proprietà El è l'id dell'elemento di partenza di Vue
+    data: {
+        message: "ciao",
+        imgPath: 'img/01.jpg', 
     }
-)
+
+})
+    
+    
+    
